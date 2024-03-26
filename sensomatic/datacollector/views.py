ad204dce5dd539850354200ef95702e33a590f19
Gustav Nybro <gustav@gustavnybro.com>
2024-03-26 12:39:01 +0100

    Added gateway api endpoint

--- sensomatic/datacollector/views.py
+++ sensomatic/datacollector/views.py
@@ -1,3 +1,16 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+@csrf_exempt
+def gateway(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+
+        return HttpResponse("")
