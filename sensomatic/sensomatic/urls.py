ddb59a15874fe6b65457e0528ca961a0c5d31f40
MadelaineNatasha <138402197+MadelaineNatasha@users.noreply.github.com>
2024-04-03 12:20:51 +0200

    ting er slettet og migration lavet

--- sensomatic/sensomatic/urls.py
+++ sensomatic/sensomatic/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('operations/', include('operations.urls')),
     path('api/', include('datacollector.urls')),
