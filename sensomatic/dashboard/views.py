d4d7c5dd708220fa65598d8ed1494a6fdafd61d7
MadelaineNatasha <138402197+MadelaineNatasha@users.noreply.github.com>
2024-04-04 14:19:51 +0200

    Display af adresser virker ikke helt, men derhen ad

--- sensomatic/dashboard/views.py
+++ sensomatic/dashboard/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth import login, authenticate, logout
 from . import forms
 from django.shortcuts import redirect
+from operations.models import Route
+import json
 
 # Create your views here.
 def index_login_page(request):
@@ -23,7 +25,11 @@ def index_login_page(request):
 
 def dashboard(request):
     if request.user.is_authenticated:
-        return render(request, 'dashboard.html')
+        routes = Route.objects.all().filter(completed=False)
+        for i in routes:
+            adresslist = i.adresses["adresses"]
+            print(adresslist)
+        return render(request, 'dashboard.html', context= {'routes':routes, 'adresslist': adresslist})
     else:
         return redirect('index')
 
@@ -31,3 +37,4 @@ def logout_user(request):
     if request.method == 'POST':
         logout(request)
         return redirect('index')
+    
