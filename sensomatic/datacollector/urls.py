623652cdbd04bbf0885080e786a1866c0606f128
mholms23 <mholms23@student.aau.dk>
2024-04-09 09:39:30 +0200

    Spelling error in urls.py handle_post name

--- sensomatic/datacollector/urls.py
+++ sensomatic/datacollector/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('handle_post/', views.handle_post, name='handdle_post')
+    path('handle_post/', views.handle_post, name='handle_post')
 ]
