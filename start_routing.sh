be95827c1fc254319f0bd1b0a7a52820fa7e3d90
Gustav Nybro <gustav@gustavnybro.com>
2024-04-30 10:55:39 +0200

    Update service file to use shellscript

---
+++ start_routing.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+/usr/bin/java -D"dw.graphhopper.datareader.file=/home/jetson/graphhopper/den.pbf" -jar /home/jetson/graphhopper/graphhopper*.jar server /home/jetson/graphhopper/config-example.yml
