349093267f291c479deadddbdef61e32a3e06676
Gustav Nybro <gustav@gustavnybro.com>
2024-04-30 14:01:13 +0200

    Move config to graphhopper

---
+++ graphhopper/start_routing.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/java -jar /home/jetson/graphhopper/graphhopper*.jar server /home/jetson/Sensomatic/graphhopper/config.yml
--- start_routing.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/java -jar /home/jetson/graphhopper/graphhopper*.jar server /home/jetson/Sensomatic/config.yml
