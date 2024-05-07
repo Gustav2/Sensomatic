10a01e77eb047a49db5232032efffe4e191a4451
jeppe sørensen <jeppe@rudihs.dk>
2024-05-07 07:47:43 +0200

    Finel test of greddy_2_opt (20)

--- Sort_algo/perform_test.py
+++ Sort_algo/perform_test.py
@@ -1,7 +1,7 @@
 import Greddy_2_opt
 
-Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_15.txt")
-Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_15.txt")
-Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_15.txt")
-Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_15.txt")
-Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_15.txt")
+Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_20.txt")
+Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_20.txt")
+Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_20.txt")
+Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_20.txt")
+Greddy_2_opt.main("Sort_algo\Coordinates_for_test\coordinates_20.txt")
