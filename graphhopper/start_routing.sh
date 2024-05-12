ff3df392246ca16c3b9220cb2d1c58a37113d3f6
Gustav <gustav@gustavnybro.com>
2024-05-12 23:36:02 +0200

    Updated config files to ubuntu default user

--- graphhopper/start_routing.sh
+++ graphhopper/start_routing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/java -jar /home/jetson/graphhopper/graphhopper*.jar server /home/jetson/Sensomatic/graphhopper/config.yml
+/usr/bin/java -jar /home/ubuntu/graphhopper/graphhopper*.jar server /home/ubuntu/Sensomatic/graphhopper/config.yml
