fe9dc3c71adf8a84610243c243160826a7af038d
Gustav <gustav@gustavnybro.com>
2024-05-16 00:21:06 +0200

    docstring for dumb dumbs

--- sensomatic/sensomatic/celery.py
+++ sensomatic/sensomatic/celery.py
@@ -11,6 +11,15 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
+"""
+This requires celery beat, so dont be a dumb dumb and remember to run it 
+with the following command: 
+
+celery -A sensomatic worker -l INFO -B
+
+The "-B" flag is for the beat scheduler, so dont forget it idiot.
+And remember API key
+"""
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
