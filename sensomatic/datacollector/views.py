a07fa90b9bd0da8b2e8787c4f21663f26b5d61d8
Gustav Nybro <gustav@gustavnybro.com>
2024-05-16 10:12:13 +0200

    maybe fix import

--- sensomatic/datacollector/views.py
+++ sensomatic/datacollector/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import SensorData, TrashIsland, Trashcan
-import greedy_2_opt
+import .greedy_2_opt
 
 
 
