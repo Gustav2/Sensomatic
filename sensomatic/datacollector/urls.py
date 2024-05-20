62c0370a3667e9205b2543520bacbedc30ab44ec
Gustav Nybro <gustav@gustavnybro.com>
2024-05-20 12:44:03 +0200

    Added code for sorting route with Graphhopper

--- sensomatic/datacollector/urls.py
+++ sensomatic/datacollector/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('handle_post/', views.handle_post, name='handle_post'),
     path('sorting_algorithm/', views.sorting_algorithm, name='sorting_algorithm'),
     path('import_addresses/', views.import_addresses, name='import_addresses'),
+    path('api_sort/', views.api_sort, name='api_sort'),
 ]
--- sensomatic/datacollector/views.py
+++ sensomatic/datacollector/views.py
@@ -1,6 +1,9 @@
 import csv
 import json
+from datetime import datetime
+
 import geopy
+import requests
 
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -8,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import SensorData, TrashIsland, Trashcan
 from .greedy_2_opt import Run
-
+from operations.models import Route
 
 
 # Create your views here.
@@ -50,6 +53,80 @@ def sorting_algorithm(request):
         Run()
         return JsonResponse({'message': 'Sorting algorithm executed'}, status = 200)
     
+
+@csrf_exempt
+def api_sort(request):
+    if request.method == "GET":
+        """
+        :param trashcans: list of Trashcan objects
+        """
+
+        trashcans = Trashcan.objects.all()
+
+        url = "https://graphhopper.com/api/1/vrp"
+
+        query = {}
+        query["key"] = ""
+
+        headers = {"Content-Type": "application/json"}
+
+        payload = {
+            "vehicles": [
+                {
+                    "vehicle_id": "truck",
+                    "start_address": {
+                        "location_id": "home",
+                        "lon": 10.0146,
+                        "lat": 57.0237
+                    },
+                    "return_to_depot": True,
+                },
+            ],
+            "configuration": {
+                "routing": {
+                    "calc_points": False,
+                    "snap_preventions": [
+                        "motorway",
+                        "trunk",
+                        "tunnel",
+                        "bridge",
+                        "ferry"
+                    ]
+                }
+            }
+        }
+
+        for trashcan in trashcans:
+            if payload.get("services") is None:
+                payload["services"] = []
+            payload["services"].append({
+                "id": f"s-{trashcan.id}",
+                "address": {
+                    "location_id": f"{trashcan.island.longitude}_{trashcan.island.latitude}",
+                    "lon": trashcan.island.longitude,
+                    "lat": trashcan.island.latitude
+                },
+            })
+
+        response = requests.post(url, json=payload, headers=headers, params=query)
+
+        data = response.json()
+        print(json.dumps(data, indent=2))
+
+        address_string = ""
+
+        for address in data["solution"]["routes"][0]["activities"]:
+            address_string += f"{address['address']['lon']},{address['address']['lat']};"
+
+        print(address_string)
+
+        Route.objects.create(
+            user=None,
+            route_name="Rute 1",
+            adresses=address_string,
+            operating_date=datetime.now().date(),
+        )
+        return JsonResponse({'message': 'Route created'}, status = 200)
 
 
 @csrf_exempt
