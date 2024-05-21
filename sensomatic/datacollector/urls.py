8b776141ebcf1c3880c62708edb537cd6a9d0264
Gustav Nybro <gustav@gustavnybro.com>
2024-05-21 12:43:28 +0200

    Added url for all_route

--- sensomatic/datacollector/urls.py
+++ sensomatic/datacollector/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('sorting_algorithm/', views.sorting_algorithm, name='sorting_algorithm'),
     path('import_addresses/', views.import_addresses, name='import_addresses'),
     path('api_sort/', views.api_sort, name='api_sort'),
+    path('all_route/', views.all_route, name='all_route'),
 ]
